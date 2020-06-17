f0aee02d6be81ca5ccf390b9ef9940db2c1cb354
Michael Tirona <tironamichael@gmail.com>
2020-06-17 11:17:23 -0700

    Added code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+let movies = ['Ghostbusters', 'Back to the Future', 'Scott Pilgrim vs. The World', 'Hot Fuzz', 'La La Land', 'Spider-Man: Into The Spider-Verse', 'It Follows', 'Your Name.', 'The Social Network', 'Shaun of the Dead']
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <h1>My Favorites Movies!</h1>
+        <hr/>
+        <h3>Here's a curated list of my favorite movies:</h3>
+        <div className="card col-sm-6" style={{maxWidth: '18rem' }}>
+          <ol className="list-group list-group-flush">
+          {
+            movies.map(movie => <li className="list-group-item">{movie}</li>)
+          }
+          </ol>
+        </div>
+    </>
   );
 }
 
